7ecd4dbbdc886739f913542731e1d5fba455ee67
pravi4uasan <39478176+pravi4uasan@users.noreply.github.com>
2020-11-25 22:00:06 +0530

    Discovery client changes
    Discovery client changes

--- src/main/java/com/tech/ProductApplication.java
+++ src/main/java/com/tech/ProductApplication.java
@@ -5,11 +5,13 @@ import java.time.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductApplication {
 
 	public static void main(String[] args) {
@@ -20,7 +22,7 @@ public class ProductApplication {
 	/*
 	 * @Bean public RestTemplate getRestTemplate() { return new RestTemplate(); }
 	 */
-	
+	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 	 
